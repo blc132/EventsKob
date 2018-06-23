33fb97709895d929a7065db9db39ba4401c10a1d
blc132 <pawelkob97@wp.pl>
2018-06-23 23:32:50 +0200

    models added: event, genre

---
+++ EventsKob/EventsKob/Migrations/201806232132171_Event and Genre.cs
@@ -0,0 +1,16 @@
+namespace EventsKob.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EventandGenre : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ EventsKob/EventsKob/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventsKob.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public ApplicationUser EventMaker { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
