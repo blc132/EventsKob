1ee0f9eb1104f7c05c38589e19085ab7aabb3d47
blc132 <pawelkob97@wp.pl>
2018-06-24 13:44:30 +0200

    create 'add event' form

---
+++ EventsKob/EventsKob/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EventsKob.Models;
+using EventsKob.ViewModels;
+
+namespace EventsKob.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventsKob/EventsKob/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EventsKob.Models;
+
+namespace EventsKob.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
