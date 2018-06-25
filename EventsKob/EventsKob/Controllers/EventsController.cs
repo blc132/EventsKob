a302657197a8c9fd9613510caa666d756da538d2
blc132 <pawelkob97@wp.pl>
2018-06-25 02:10:37 +0200

    added adding Event

--- EventsKob/EventsKob/Controllers/EventsController.cs
+++ EventsKob/EventsKob/Controllers/EventsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using EventsKob.Models;
 using EventsKob.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace EventsKob.Controllers
 {
@@ -21,6 +23,25 @@ namespace EventsKob.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(EventFormViewModel viewModel)
+        {
+            var eventMakerId = User.Identity.GetUserId();
+            var eventMaker = _context.Users.Single(e => e.Id == eventMakerId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var eventToAdd = new Event
+            {
+                EventMaker = eventMaker,
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                Genre = genre,
+                Venue = viewModel.Venue,
+            };
+            _context.Events.Add((eventToAdd));
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
