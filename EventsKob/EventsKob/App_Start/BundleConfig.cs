61457d669cf93711156b1b45d90f71e76c7fee37
blc132 <pawelkob97@wp.pl>
2018-08-28 23:55:59 +0200

    added new folder for css

--- EventsKob/EventsKob/App_Start/BundleConfig.cs
+++ EventsKob/EventsKob/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace EventsKob
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Styles/site.css"));
         }
     }
 }
