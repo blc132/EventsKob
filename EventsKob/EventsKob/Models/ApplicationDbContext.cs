c1ccfb0e6a0ccd97983321ea3dad398cc24c65cc
blc132 <pawelkob97@wp.pl>
2018-08-30 10:54:19 +0200

    added new model/table attendance

--- EventsKob/EventsKob/Models/ApplicationDbContext.cs
+++ EventsKob/EventsKob/Models/ApplicationDbContext.cs
@@ -6,7 +6,9 @@ namespace EventsKob.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Event> Events { get; set; }
-        public DbSet<Genre> Genres { get; set; }   
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -15,6 +17,16 @@ namespace EventsKob.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Event)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
